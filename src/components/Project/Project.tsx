74c087fe94e7f0832cffb0d27921045963c3683f
Karthi <karthikkrishna230104@gmail.com>
2025-06-11 22:08:51 +0530

    Update projects

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -99,7 +99,7 @@ export function Project() {
             <header>
               <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round"> <title>Folder</title> <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path> </svg>
               <div className="project-links">
-                <a href="https://github.com/CodeVinayak/Serverless-Voting-Application" target="_blank" rel="noreferrer">
+                <a href="https://github.com/mkk-1817/Netflix-clone" target="_blank" rel="noreferrer">
                   <img src={githubIcon} alt="Visit site" /></a>
                 {/* <a href="https://vote.vinayaksingh.com" target="_blank" rel="noreferrer">
                   <img src={externalLink} alt="Visit site" />
@@ -122,7 +122,7 @@ export function Project() {
                 <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
               </svg>
               <div className="project-links">
-                <a href="" target="_blank" rel="noreferrer">
+                <a href="https://github.com/mkk-1817/HR-Analytics" target="_blank" rel="noreferrer">
                   <img src={githubIcon} alt="Visit site" />
                 </a>
                 {/* <a href="https://youtu.be/NOm-3MynPLE" target="_blank" rel="noreferrer">
